0c2c1f4be7b99945e2d744a1f8c7eee1ea545b35
preawbnp <preawbnp@preawbnps-MacBook-Pro.local>
2018-04-19 14:44:13 +0700

    Set up project

---
+++ consts/firebase_info.js
@@ -0,0 +1,31 @@
+export const config = {
+    apiKey: "AIzaSyB8IXkB-kGzYZYAlwauPbvl6ZbczJx36Vg",
+    authDomain: "my-app-8c4ab.firebaseapp.com",
+    databaseURL: "https://my-app-8c4ab.firebaseio.com",
+    projectId: "my-app-8c4ab",
+    storageBucket: "my-app-8c4ab.appspot.com",
+    messagingSenderId: "20489091490"
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const config = {
+//   apiKey: "AIzaSyDaY9hbGQOr41ljQeZgX26NTd_SqKwH4oQ",
+//   authDomain: "studentloan-7fbce.firebaseapp.com",
+//   databaseURL: "https://studentloan-7fbce.firebaseio.com",
+//   projectId: "studentloan-7fbce",
+//   storageBucket: "studentloan-7fbce.appspot.com",
+//   messagingSenderId: "1016135551882"
+// };
